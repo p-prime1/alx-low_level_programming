03955181a74a2efc5ba18f5886a353fa928d82f5
p-prime1 <preciousnwaka95@gmail.com>
2022-10-16 11:08:18 +0100

    g

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <sdio.h>
+
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome iss a piece of art,\n");
+	return (0);
+}
