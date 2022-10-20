bef4a8363c975a67e51909e68669286b979c598a
p-prime1 <preciousnwaka95@gmail.com>
2022-10-20 15:29:19 +0100

    m

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,8 +5,13 @@
  * Return: Always 0
  */
 void print_alphabet(void);
-	for (int x = 'a'; x < 'z'; x++)
+int main(void)
+{
+	for(int x = 'a'; x < 'z'; x++)
 	{
+		char small = tolower(x);
 		putchar(small);
 	}
 	return (0);
+}
+
