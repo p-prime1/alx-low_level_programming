f2299cdc069e728f06c8947174ea8c6affca8354
p-prime1 <preciousnwaka95@gmail.com>
2022-12-11 07:52:24 +0100

    Yess

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints numbers from 1 to 10
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	n = 1;
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
