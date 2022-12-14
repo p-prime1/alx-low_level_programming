4d96978c110912daee34e4226bc8a31d7584f578
p-prime1 <preciousnwaka95@gmail.com>
2022-12-14 17:58:12 +0100

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int i;
+
+	x = 1;
+	while (x <= 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			putchar(i);
+	}
+	x++;
+}
+
