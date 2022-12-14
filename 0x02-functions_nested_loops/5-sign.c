db38d5f82d0bd1991ba5829f5b8c5d270c2205df
p-prime1 <preciousnwaka95@gmail.com>
2022-12-14 22:22:19 +0100

    mm

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign(int n)- checks for positive or negative
+ * @n: Int;
+ * Return: returns positive or negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(n + '0');
+		return (0);
+	}
+	else
+	{
+		putchar("-");
+		return (-1);
+	}
