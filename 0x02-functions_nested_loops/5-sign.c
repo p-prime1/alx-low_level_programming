e8b2d127590b8bd7d5678cccfb57e83a81f069ad
p-prime1 <preciousnwaka95@gmail.com>
2022-12-14 22:23:38 +0100

    mm

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar("+");
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
@@ -20,6 +20,6 @@ int print_sign(int n)
 	}
 	else
 	{
-		putchar("-");
+		putchar('-');
 		return (-1);
 	}
