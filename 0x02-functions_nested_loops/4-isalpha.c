e8af4d52d1a6257ec5c8f8b8dd2e8c68b5297c54
p-prime1 <preciousnwaka95@gmail.com>
2022-12-14 22:10:53 +0100

    m

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for lowercase
+ * @c: Argument
+ * Return: 1 or 0
+ */
+
+int _isalpha(int c)
+{
+	if (c != 1)
+	{
+		return (0);
+	}
+	else if (c == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
