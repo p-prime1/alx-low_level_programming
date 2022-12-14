1ad85f42df460b3db085f6706bf33663e36a83a2
p-prime1 <preciousnwaka95@gmail.com>
2022-12-14 21:20:44 +0100

    n

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - Checks if int is in lower case
+ * @c: Upper or Lower case
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (c != 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
