40a34398dcf5848ead0098132d4ea489037a0e96
p-prime1 <preciousnwaka95@gmail.com>
2022-12-15 22:18:30 +0100

    a

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,8 @@
-#ifndef MAIN.H
-#define MAIN.H
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
+
 void positive_or_negative(int );
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
