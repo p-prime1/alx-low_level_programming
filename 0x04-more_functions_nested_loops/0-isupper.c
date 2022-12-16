903c42b8cb615169dd96fea3697508cfb993de72
p-prime1 <preciousnwaka95@gmail.com>
2022-12-16 22:43:57 +0100

    I don tire

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,19 @@
 #include  "main.h"
 
 /**
- * main - Checks for uppercase
+ * _isupper - Checks for uppercase
+ * @c: Letter
  * Return: Returns 0 or 1
  */
 
-int main(void)
+int _isupper(int c)
 {
-	int _isupper(int c)
+	if (c != 1)
 	{
-		if (c != 1)
-		{
-			return (0);
-		}
-		else
-		{
-			return (1);
-		}
+		return (0);
+	}
+	else
+	{
+	return (1);
 	}
 }
