ea560d30a40e455a3b8bb88bdbf537b69d65043b
p-prime1 <preciousnwaka95@gmail.com>
2022-12-17 10:01:28 +0100

    I

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Multiply two numbers
+ * @int a: First integer
+ * @int b: Second integer
+ * Return: Return multiple
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,5 +8,6 @@
 int _putchar(int character);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
