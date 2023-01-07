27c4fb5817759fbf6c5583e28725f787b3205b3a
p-prime1 <preciousnwaka95@gmail.com>
2023-01-07 14:17:47 +0100

    Recursio

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,8 +1,10 @@
 #include "main.h"
 
 /**
- *
- *
+ * _pow_recursion - Raise to the power
+ * @x: Base number
+ * @y: Power
+ * Return: Returns 1 or -1 or Index
  */
 
 int _pow_recursion(int x, int y)
@@ -19,23 +21,4 @@ int _pow_recursion(int x, int y)
 	{
 		return (x * _pow_recursion(x, y - 1));
 	}
-}
-
-int main(void)
-{
-    int r;
-
-    r = _pow_recursion(1, 10);
-    printf("%d\n", r);
-    r = _pow_recursion(1024, 0);
-    printf("%d\n", r);
-    r = _pow_recursion(2, 16);
-    printf("%d\n", r);
-    r = _pow_recursion(5, 2);
-    printf("%d\n", r);
-    r = _pow_recursion(5, -2);
-    printf("%d\n", r);
-    r = _pow_recursion(-5, 3);
-    printf("%d\n", r);
-    return (0);
 }
