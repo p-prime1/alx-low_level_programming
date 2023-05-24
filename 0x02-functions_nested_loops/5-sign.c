d79910b592b5186c99e7824e7b963aa90e4aba8a
p-prime1 <preciousnwaka95@gmail.com>
2023-05-24 22:53:55 +0100

    Fixed is lower function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
 		return (1);
+		_putchar('+');
 	}
 	else if (n == 0)
 	{
-		putchar(n + '0');
 		return (0);
+		_putchar(n + '0');
 	}
 	else
 	{
-		putchar('-');
 		return (-1);
+		_putchar('-');
 	}
 }
