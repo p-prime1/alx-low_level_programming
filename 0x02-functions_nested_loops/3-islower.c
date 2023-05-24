5875916f1911ad855fb007b266258be7b9219497
p-prime1 <preciousnwaka95@gmail.com>
2023-05-24 22:48:53 +0100

    Fixed is lower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -12,11 +12,11 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		_putchar(1 + '0');
+		return (1);
 	}
 	else
 	{
-		_putchar(0);
+		return (0);
 	}
 	return (0);
 }
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,12 @@
 
 int _isalpha(int c)
 {
-	if (c != 1)
+	if (c >= 'a' && c <= 'z')
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
