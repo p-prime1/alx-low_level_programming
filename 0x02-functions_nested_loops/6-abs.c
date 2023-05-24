b409160330b70e7a81b04456fe5d4f4de9213f48
p-prime1 <preciousnwaka95@gmail.com>
2023-05-24 23:12:03 +0100

    Fixed is lower function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Returns absolute value
+ * @r: value
+ * Return: 0;
+ */
+
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (-*r);
+	}
+	else
+	{
+		return (r);
+	}
+	return (0);
+}
