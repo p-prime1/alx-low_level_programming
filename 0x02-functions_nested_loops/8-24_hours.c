67f8a0687b92f90977aa69d6a9e4033bfc1f2c8a
p-prime1 <preciousnwaka95@gmail.com>
2023-05-25 23:28:46 +0100

    Added to isalpha

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.c"
+
+int main(void)
+{
+
+}
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour <= 24; hour++)
+	{
+		for(minute = 0; minute <= 60; minute++)
+		{
+			putchar((hour / 10) + '0');
+			putchar((hour % 10) + '0');
+			putchar(;);
+			putchar((minute % 10) + '0');
+			putchar((minute / 10) + '0');
+		}
+	}
