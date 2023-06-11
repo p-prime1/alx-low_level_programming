c10ff99e73f4917ba49b6b27b99e8c2cef0dd7d9
p-prime1 <preciousnwaka95@gmail.com>
2023-06-11 18:43:28 +0100

    Fixed strlen function

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /*
- * _strlen_recursion - recursive function.
- * @s: String ointer
+ * _strlen_recursion - Returns the number of characters in string
+ * @s: String pointer
  * Return: Always 0
  */
 
@@ -9,7 +9,7 @@ int _strlen_recursion(char *s)
 {
 
 	if (*s == '\0')
-		return(0);
+		return (0);
 	else
 		return (1 + _strlen_recursion(s + 1));
 }
