88d2e0ca7261fc12b914d752162f8f62c2c59a44
p-prime1 <preciousnwaka95@gmail.com>
2023-09-30 17:17:24 +0100

    Added main header file with prototypes

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1);
+};
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recrsion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
