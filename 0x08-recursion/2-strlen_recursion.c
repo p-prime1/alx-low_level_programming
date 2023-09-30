da551163a08238ddccc72f2d3da1c514817f9b36
p-prime1 <preciousnwaka95@gmail.com>
2023-10-01 00:13:17 +0100

    Added betty compliant

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,25 +1,24 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _strlen_recursion - returns the lenght of a sting
+ * @s: Pointer to string
+ * Return: Number
+ */
+
 int _strlen_recursion(char *s)
 {
 	int i;
 
 	i = 1;
 	if (*s == '\0')
+	{
 		return (i);
+	}
 	else
 	{
-		i++;	
-		return(1 + (_strlen_recursion(s + 1)));	
+		i++;
+		return (1 + (_strlen_recursion(s + 1)));
 	}
-}
-
-int main(void)
-{
-    int n;
-
-    n = _strlen_recursion("Corbin Coleman");
-    printf("%d\n", n);
-    return (0);
 }
