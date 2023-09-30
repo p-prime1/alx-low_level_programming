989209e61c59f7265d92175e26e5767152b8b828
p-prime1 <preciousnwaka95@gmail.com>
2023-09-30 18:38:28 +0100

    Remove return for puts function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,7 +9,7 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		break;
+		_putchar('\n');
 	}
 	else
 	{
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,7 @@
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
-};
+}
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
