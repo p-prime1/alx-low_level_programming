6b5f1fd143be2d75ad3aa047b96f3f2c86eb1020
p-prime1 <preciousnwaka95@gmail.com>
2023-09-30 18:40:09 +0100

    Declared puthar finction

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,10 +3,7 @@
 
 #include <unistd.h>
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+int _putchar(char);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
