b3b9db3c9fdafc754525a96442f758ef811d7936
p-prime1 <preciousnwaka95@gmail.com>
2023-10-05 09:31:23 +0100

    Fixed str_concat function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,13 +10,14 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	len2 = 0;
 	j = 0;
-	if (s1 || s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	while (s1[len1] == '\0')
+	while (s1[len1] != '\0')
 		len1++;
-	while (s2[len2] == '\0')
+	while (s2[len2] != '\0')
 		len2++;
 	len2 += 1;
+	//printf("%d", (len1 + len2));
 	s3 = (char *) malloc(sizeof(char) * (len1 + len2));
 	if (s3 == NULL)
 		return NULL;
