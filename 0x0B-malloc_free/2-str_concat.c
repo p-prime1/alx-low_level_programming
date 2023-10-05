6f88abcad63820fef5b7a4e0166130f4a05bf4cb
p-prime1 <preciousnwaka95@gmail.com>
2023-10-05 09:42:18 +0100

    Removed comments and added betty compliant style

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/**
+ * *str_concat - A functon to concatenate two strings
+ * @s1: first string
+ * @s2: second stringi
+ * Return: Return concatenated string
+ */
+
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
 	unsigned int i, j, len1, len2;
 
-	
 	i = 0;
 	len1 = 0;
 	len2 = 0;
@@ -17,10 +23,9 @@ char *str_concat(char *s1, char *s2)
 	while (s2[len2] != '\0')
 		len2++;
 	len2 += 1;
-	//printf("%d", (len1 + len2));
 	s3 = (char *) malloc(sizeof(char) * (len1 + len2));
 	if (s3 == NULL)
-		return NULL;
+		return (NULL);
 	while (s1[i] != '\0')
 	{
 		s3[i] = s1[i];
@@ -31,8 +36,8 @@ char *str_concat(char *s1, char *s2)
 		s3[i] = s2[j];
 		i++;
 		j++;
-		}
+	}
 	s3[i] = '\0';
 	return (s3);
-	free (s3);
+	free(s3);
 }
