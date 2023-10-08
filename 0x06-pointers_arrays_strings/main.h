52c7ebb565e34190da402c41d464ac83f0757bdd
p-prime1 <preciousnwaka95@gmail.com>
2023-10-09 00:29:38 +0100

    Added new files

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,6 +3,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+char _putchar(char c)
+{
+	return (putchar(c));
+}
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
