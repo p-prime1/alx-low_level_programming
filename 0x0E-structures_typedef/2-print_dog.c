fc71731ec6dcc30c946e4e22a785477f61b78c7d
p-prime1 <preciousnwaka95@gmail.com>
2023-10-11 00:12:39 +0100

    Added the print_dog file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - Print a struct
+ * struct dog - Sturct dor dog
+ * @d: Pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		printf(" ");
+	}
+	else
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+			printf("Age: %2f\n", d->age);
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
