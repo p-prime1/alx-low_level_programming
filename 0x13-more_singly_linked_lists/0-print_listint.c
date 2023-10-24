55ff2fbdd8b976f43e9bd284dd82b7a175756e6e
p-prime1 <preciousnwaka95@gmail.com>
2023-10-24 23:27:56 +0100

    Added adjustments to the code

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,18 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodeCount;
 
-	nodeCount = 0;
-	while (h != NULL)
+	nodeCount = 1;
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		nodeCount++;
 	}
+	printf("%d\n", h->n);
 	return (nodeCount);
 }
 
