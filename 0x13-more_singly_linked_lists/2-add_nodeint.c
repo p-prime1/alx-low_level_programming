d7630ba3131512521bb8f21042be0770f32e78b7
p-prime1 <preciousnwaka95@gmail.com>
2023-10-25 00:25:40 +0100

    Pushing pritn_listint again

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,7 +12,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newNode;
 
-	newNode = (listint_t*)malloc(sizeof(listint_t));
+	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
 	if (*head == NULL)
