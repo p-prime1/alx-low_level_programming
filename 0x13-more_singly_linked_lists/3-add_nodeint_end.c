858d049c2f055238a99fcb673e702efb5cf47267
p-prime1 <preciousnwaka95@gmail.com>
2023-10-25 21:32:34 +0100

    Added a new finction

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint_end - Adds A new node to the end of a list
+ * @head: Pointer to thr first node
+ * @n: Variable to be assigned to the node
+ * Return: Address of the new node
+ */
+
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *newNode;
+	listint_t *temp;
+
+	temp = *head;
+	newNode = malloc(sizeof(listint_t));
+	if (newNode == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	else
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+	}
+	temp->next = newNode;
+	newNode->n = n;
+	newNode->next = NULL;
+	return (newNode);
+}
+
