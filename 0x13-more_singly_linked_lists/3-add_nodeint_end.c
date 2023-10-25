58380decb6d84056398193a440972ce583346d3b
p-prime1 <preciousnwaka95@gmail.com>
2023-10-25 22:56:11 +0100

    Idon tire

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	newNode = malloc(sizeof(listint_t));
 	if (newNode == NULL)
 		return (NULL);
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		*head = newNode;
 		newNode->n  = n;
