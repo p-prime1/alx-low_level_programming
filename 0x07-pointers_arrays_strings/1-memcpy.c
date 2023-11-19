bc370060502a1153a6e50428cfda5f0a69b7b279
p-prime1 <preciousnwaka95@gmail.com>
2023-11-19 20:19:32 +0100

    Added betty compliant code and optimized the code

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,21 +3,19 @@
 /**
  * _memcpy - copies Data from one memory to another
  * @dest: Destination memory
- * @src: Source 
- *@n: Number of bytes to be copied
+ * @src: Source
+ * @n: Number of bytes to be copied
+ * Return: Returns the Address of the destination
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i;
 
 	i = 0;
-	j = 0;
-
-	while (i <= n)
+	while (i < n)
 	{
-		dest[j] = src[j];
-		j++;
+		dest[i] = src[i];
 		i++;
 	}
 	return (dest);
