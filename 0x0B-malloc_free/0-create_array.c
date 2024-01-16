98b5ed096f01687ba304ec54688ca868aa1fa322
p-prime1 <preciousnwaka95@gmail.com>
2024-01-16 11:20:43 +0100

    Added a condition to check for size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,6 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	i = 0;
+	if (size == 0)
+		reutrn (NULL);
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
