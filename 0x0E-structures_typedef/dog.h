f3edc5dc18abc7debbcd603a43d11b25439e1722
p-prime1 <preciousnwaka95@gmail.com>
2024-01-24 20:47:54 +0100

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * dog - A struct giving information of a dog
+ * @name: Nemae of yhe dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
