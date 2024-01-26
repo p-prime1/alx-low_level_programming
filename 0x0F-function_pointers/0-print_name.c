c92c50a9025a1cf27724a9f4b77ab8bb747b24c4
p-prime1 <preciousnwaka95@gmail.com>
2024-01-26 22:53:45 +0100

    f

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-#include "function_pointers"
+#include "function_pointers.h"
 
 /**
  * print_name - A function that prints a name
@@ -9,5 +9,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-		return (f(name));
+		(f(name));
 }
