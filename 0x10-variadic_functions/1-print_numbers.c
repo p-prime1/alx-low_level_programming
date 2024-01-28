104aa71143fae14e531a2d826616adad28284692
p-prime1 <preciousnwaka95@gmail.com>
2024-01-28 22:14:08 +0100

    D

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_number - Prints number passed as arguments
+ * @seperator: Char to seperate numbers
+ * @n: Number of args passed
+ */
+void print_numbers(const char *seperator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int x;
+
+	va_list args;
+	va_start(args, n);
+
+	if (seperator == NULL)
+	{
+		for (i = 0; i < n; i++)
+		{
+			x = va_arg(args, int);
+			printf("%d ", x);
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			x = va_arg(args, int);
+			printf("%d", x);
+			if (i != (n- 1))
+				printf("%c ", *seperator);
+		}
+		printf("\n");
+	}
+	va_end(args);
+}
