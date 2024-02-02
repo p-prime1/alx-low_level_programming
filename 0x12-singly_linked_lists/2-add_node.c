86b88c3b6c9dbd08d635bd22e3cd1f11115c67a0
p-prime1 <preciousnwaka95@gmail.com>
2024-02-02 14:51:16 +0100

    Added second func

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to the begining of a list
+ * @head: Head of the list
+ * @str: String to beadded to the string
+ * Return: Returns NULL on failure or returns the new node
+ */
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *string;
+	size_t str_len;
+	struct list_s *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	str_len = 0;
+
+	string = strdup(str);
+	if (new_node == NULL || string == NULL || head == NULL)
+		return (NULL);
+	while (string[str_len] != '\0')
+	{
+		str_len++;
+	}
+	new_node->str = string;
+	new_node->len = str_len;
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
+}
