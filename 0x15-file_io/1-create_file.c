74721d17829d0fc0dd549f89c1e6fea8be2715e6
p-prime1 <preciousnwaka95@gmail.com>
2024-02-15 22:26:19 +0100

    Changed file permission and added betty style comments

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
+/**
+ * create_file - Creates a file
+ * @filename: Name of file to be created
+ * @text_content: Content to be written to file
+ * Return: Returns -1 on success or 1 on failure
+ */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_WRONLY | O_RDONLY, 700);
+	fd = open(filename, O_CREAT | O_WRONLY | O_RDONLY, 600);
 	if (fd == -1)
 	{
 		if (errno == EEXIST)
