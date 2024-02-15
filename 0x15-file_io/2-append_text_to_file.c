f5865a2c6a836ed1a6d0e5b328cec70fcc8b669d
p-prime1 <preciousnwaka95@gmail.com>
2024-02-15 22:56:05 +0100

    Declared i variable

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,15 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - Appends text to a file
+ * @filename: Name of file to be appended
+ * @text_content: Content to be appended to the file
+ * Return: Returns -1 on failure on failure and 1 on success
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, i;
 
 	if (filename == NULL)
 		return (-1);
