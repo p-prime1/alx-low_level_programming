e3605f34fc156156412bd4f2d0d216a7faf19031
p-prime1 <preciousnwaka95@gmail.com>
2024-03-23 00:25:17 +0100

    Adds betty style description

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - Sums elements in a list
+ * @head: Pointer to header of list
+ * Return: Returns 0 on failure and sum of all elements on success
+ */
+
 int sum_dlistint(dlistint_t *head)
 {
 	int sum;
