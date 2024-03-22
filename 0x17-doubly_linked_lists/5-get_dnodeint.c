fc22dab2434e73bb243af4426b8b85e4ef0ad0d6
p-prime1 <preciousnwaka95@gmail.com>
2024-03-23 00:16:17 +0100

    Used betty compliant code

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - Returns the node at the nth index
+ * @head: Pointer to head of a node
+ * @index: The nth index
+ * Return: Returns NULL on failure or the nth Node on success
+ */
+
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
