10fb9e15e5229d9f341e2a5c561ebe8dab0b27d9
p-prime1 <preciousnwaka95@gmail.com>
2024-04-17 12:46:59 +0100

    Second commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,13 +1,20 @@
 #include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a new Table
+ * @size: Size of table
+ * Return: Returns the new table on success or NULL on failure
+ */
+
 hash_table_t  *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
 
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t) * size);
 	if (new_table == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_table->size = size;
-	return new_table;
+	return (new_table);
 }
