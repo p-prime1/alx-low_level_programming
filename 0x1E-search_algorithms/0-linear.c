1994b427a099a215c3ed1be33b45b7ee75a2a3a6
p-prime1 <preciousnwaka95@gmail.com>
2024-07-16 20:51:51 +0100

    First commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,26 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - Searches an array linearly
+ * @array: Array to be searched
+ * @size: Size of the array
+ * @value: Value to be searched
+ * Return: Returns index of the value on success or -1 on failure
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+	i = 0;
+	while (i < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
